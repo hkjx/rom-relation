1916e0b9e672f5e5cca2927c4c6a555514badb7e
hkjx <hkjxaa@gmail.com>
2013-10-10 14:20:27 +0300

    D

--- lib/rom/support/axiom/adapter/mongo.rb
+++ lib/rom/support/axiom/adapter/mongo.rb
@@ -1,6 +1,6 @@
 # encoding: utf-8
 
-require 'axiom-mongo-adapter'
+# require 'axiom-mongo-adapter'
 
 require 'rom/support/axiom/adapter'
 require 'rom/support/axiom/adapter/data_objects'
@@ -8,12 +8,12 @@ require 'rom/support/axiom/adapter/data_objects'
 module Axiom
   module Adapter
 
-    # A Axiom adapter for postgres
+    # A Axiom adapter for mongo
     #
     class Mongo < DataObjects
 
       uri_scheme :mongo
 
-    end # class Postgres
+    end # class Mongo
   end # module Adapter
 end # module Axiom
